cf5c9f9babb1a88daa5f09f1b5f1721fd21b46ef
Erik Goulart <erikcgoulart@hotmail.com>
2025-11-09 13:23:41 -0300

    CreateInsert1.sql

--- CreateInsert1.sql
+++ CreateInsert1.sql
@@ -1,15 +1,17 @@
+--Criando as Tabelas
+
 CREATE TABLE Clientes (
     IdCliente INT PRIMARY KEY,
     Nome VARCHAR(100),
     Cidade VARCHAR(50)
 );
-
+GO
 CREATE TABLE Produtos (
     IdProduto INT PRIMARY KEY,
     NomeProduto VARCHAR(100),
     Preco DECIMAL(10,2)
 );
-
+GO
 CREATE TABLE Vendas (
     IdVenda INT PRIMARY KEY,
     IdCliente INT,
@@ -19,20 +21,24 @@ CREATE TABLE Vendas (
     FOREIGN KEY (IdCliente) REFERENCES Clientes(IdCliente),
     FOREIGN KEY (IdProduto) REFERENCES Produtos(IdProduto)
 );
+GO
+    
+--Inserindo Dados
 
 INSERT INTO Clientes VALUES
-(1, 'Ana Souza', 'São Paulo'),
+(1, 'Ana Souza', 'SÃ£o Paulo'),
 (2, 'Carlos Lima', 'Rio de Janeiro'),
-(3, 'Beatriz Ramos', 'São Paulo');
-
+(3, 'Beatriz Ramos', 'SÃ£o Paulo');
+GO
 INSERT INTO Produtos VALUES
 (1, 'Notebook', 3500.00),
 (2, 'Mouse', 80.00),
 (3, 'Teclado', 150.00);
-
+GO
 INSERT INTO Vendas VALUES
 (1, 1, 1, 1, '2025-11-01'),
 (2, 1, 2, 2, '2025-11-01'),
 (3, 2, 3, 1, '2025-11-02'),
 (4, 3, 1, 1, '2025-11-02'),
 (5, 3, 2, 1, '2025-11-03');
+GO
